5b3c58396f788608312fbef4bfde44bdf82a49cd
esdmr <esdmr0@gmail.com>
2025-07-21 13:49:23 +0330

    Update

--- exercise.py
+++ exercise.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+title = 'Whatever Social'
+
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'johndoe'}
+    posts = [
+        {
+            'author': {'username': 'johndoe'},
+            'content': 'lorem ipsum',
+        },
+        {
+            'author': {'username': 'johndoe'},
+            'content': 'something something',
+        },
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
